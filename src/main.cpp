a894e7c2591010c87309691cbf1e853d78e2deb3
Pavel <sidorovich.pasha@gmail.com>
2022-01-13 22:46:22 +0300

    basic impl

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
+#include <array>
 
 #include "to_string.hpp"
 
-
+class Class1
+{
+	int mAge;
+	std::string mName;
+public:
+	Class1(std::string&& _Name, int _Age)
+		:
+		mAge(_Age), mName(std::move(_Name)) {}
+	std::string to_string()
+	{
+		return mName + ":" + std::to_string(mAge);
+	}
+	std::string to_string() const
+	{
+		return mName + ":" + std::to_string(mAge);
+	}
+};
 
 int main()
 {
+	Class1 per1("RayeS", 20);
+	Class1 per2("Pavel", 20);
+	std::cout << ext::to_string(per1) << " and " << ext::to_string(per2) << std::endl;
+
+	const char* hi = "Hello, ";
+	const char wo[] = "world!!!";
+	std::string str("(c) youre Traitor");
+	std::cout << ext::to_string(hi) << ext::to_string(wo) << ext::to_string(str) << std::endl;
+
+	std::array<int, 5> a{ 1,2,3,4,5 };
+	std::cout << ext::to_string(a) << std::endl;
+
+	std::cout << ext::to_string(21.5f) << " " << ext::to_string(190) << " " << ext::to_string(true) << std::endl;
+	std::cout << ext::to_string(21.5f, 190, true) << std::endl;
+
 
 	return 0;
 }
--- src/to_string.hpp
+++ src/to_string.hpp
@@ -5,11 +5,57 @@
 
 namespace ext
 {
-
 	template<class T>
-	auto to_string(const T& _Obj)
+	auto to_string(const T& _Obj) -> decltype(std::declval<T>().to_string())
 	{
 		return _Obj.to_string();
+	}
+
+	template<class T>
+	auto to_string(T& _Val) -> 
+		std::enable_if_t<std::is_convertible_v<T, std::string>, std::string>
+	{
+		return std::string(_Val);
+	}
+
+	template<class T>
+	auto to_string(T&& _Val) ->
+		std::enable_if_t<std::is_convertible_v<T, std::string>, std::string>
+	{
+		return std::string(std::move(_Val));
+	}
+
+	template<class T>
+	auto to_string(T& _Val) -> std::enable_if_t<std::is_convertible_v<T, int>, std::string>
+	{
+		return ::std::to_string(_Val);
+	}
+
+	template<class T>
+	auto to_string(T&& _Val) -> std::enable_if_t<std::is_convertible_v<T, int>, std::string>
+	{
+		return ::std::to_string(std::move(_Val));
+	}
+
+	template<class T>
+	auto to_string(const T& _Conteiner) -> 
+		decltype(::ext::to_string(std::declval<typename T::iterator::value_type>()))
+	{
+		std::string result;
+		for (auto&& elem : _Conteiner)
+		{
+			if (!result.empty())
+				result += ", ";
+			result += ::ext::to_string(elem);
+		}
+		result += ";";
+		return result;
+	}
+
+	template<class... Args>
+	auto to_string(Args&&... args) -> std::enable_if_t<(sizeof...(Args) > 1), std::string>
+	{
+		return (... += (::ext::to_string(std::forward<Args>(args)) + " "));
 	}
 
 
